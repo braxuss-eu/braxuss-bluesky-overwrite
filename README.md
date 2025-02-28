532c3b551f835e25773336345392b08cc6fffe60
braxuss <github@braxuss.eu>
2025-02-28 20:39:29 +0100

    License, readme and link to github

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # bluesky-overwrite
 
-This template should help get you started developing with Vue 3 in Vite.
+If you want to edit a post without losing it's position in a thread or the answers you can overwrite it with this tool:
 
-## Recommended IDE Setup
+1. Start by creating a new post with the content that you want to use. That will be the "source" post.
+2. This tool will overwrite the "target" post with the content of the "source" post when you say so.
+3. Then you can delete the "source" one, it's no longer needed.
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+## Ready to use
 
-## Customize configuration
+https://overwrite.braxuss.eu
 
-See [Vite Configuration Reference](https://vite.dev/config/).
+## Acknowledgements
+
+This project is heavily inspired by the famous tool https://pdsls.dev.
+I just wanted to make it simpler for non geeks to edit posts with facets and resources.
+
+I also used https://github.com/mary-ext/atcute as Bluesky client library and also for oauth login.
 
 ## Project Setup
 
@@ -24,12 +31,8 @@ npm run dev
 
 ### Compile and Minify for Production
 
+Remember to change `client-metadata.json` so it reflects your domain name.
+
 ```sh
 npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
 ```
--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-      <h1>Overwrite a Bluesky post</h1>
+      <h1>Overwrite a Bluesky post <a href="https://github.com/braxuss-eu/braxuss-bluesky-overwrite" class="i-github" target="_blank"></a></h1>
       <h2>with the content of another one</h2>
     </div>
   </header>
@@ -43,5 +43,17 @@ footer {
   grid-area: footer;
   padding: 0.5rem;
   text-align: center;
+}
+
+a.i-github {
+  display: inline-block;
+  mask: url("data:image/svg+xml;utf8,%3Csvg viewBox='0 0 16 16' width='1.2em' height='1.2em' xmlns='http://www.w3.org/2000/svg' %3E%3Cpath fill='currentColor' d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59c.4.07.55-.17.55-.38c0-.19-.01-.82-.01-1.49c-2.01.37-2.53-.49-2.69-.94c-.09-.23-.48-.94-.82-1.13c-.28-.15-.68-.52-.01-.53c.63-.01 1.08.58 1.23.82c.72 1.21 1.87.87 2.33.66c.07-.52.28-.87.51-1.07c-1.78-.2-3.64-.89-3.64-3.95c0-.87.31-1.59.82-2.15c-.08-.2-.36-1.02.08-2.12c0 0 .67-.21 2.2.82c.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82c.44 1.1.16 1.92.08 2.12c.51.56.82 1.27.82 2.15c0 3.07-1.87 3.75-3.65 3.95c.29.25.54.73.54 1.48c0 1.07-.01 1.93-.01 2.2c0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8'/%3E%3C/svg%3E") no-repeat;
+  mask-size: 100% 100%;
+  background-color: currentColor;
+  color: light-dark(#333b3c, #efefec);
+  filter: drop-shadow(silver 0px 0px 5px);
+  width: 2rem;
+  height: 2rem;
+  margin-left: 0.5rem;
 }
 </style>
